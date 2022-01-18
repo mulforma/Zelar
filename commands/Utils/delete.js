7d29f4ce214e64ea27e60c496a401622ac87e4c0
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-01-18 09:30:16 +0700

    feat: Add delete and trivia

---
+++ commands/Utils/delete.js
@@ -0,0 +1,71 @@
+// Import SlashCommandBuilder
+const { SlashCommandBuilder } = require("@discordjs/builders");
+// Import Permissions from discord.js
+const { Permissions } = require("discord.js");
+
+// Export command
+module.exports = {
+  // Command data
+  data: new SlashCommandBuilder()
+    // Command name
+    .setName("delete")
+    // Command description
+    .setDescription("Delete message within 2 weeks old")
+    // Add number option
+    .addNumberOption((option) =>
+      option
+        // Set option name
+        .setName("amount")
+        // Set option description
+        .setDescription("Amount of message to  delete, limit 100 message")
+        // Set option is required
+        .setRequired(true)
+    ),
+  // Command category
+  category: "Utils",
+  // Command run function
+  /**
+   * @param {import('discord.js').Client} client
+   * @param {import('discord.js').CommandInteraction} interaction
+   * @returns {Promise<void>}
+   */
+  async execute (client, interaction) {
+    // Check if user has permission to use command
+    if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+      // Send error message
+      return await interaction.reply({
+        content: "You can't use this command because you don't have permission to manage message.",
+        ephemeral: true,
+      });
+    }
+    // Get amount from option
+    const amount = interaction.options.getNumber("amount");
+    
+    // Check if amount is more than 100
+    if (amount > 100) {
+      // Send error message
+      return await interaction.reply({
+        content: "Amount must not be higher than 100.",
+        ephemeral: true,
+      });
+    }
+    // If amount is less than or equal to 0
+    else if (amount <= 0) {
+      // Send error message
+      return await interaction.reply({
+        content: "Amount must not be equal to or less than zero.",
+        ephemeral: true,
+      });
+    }
+    
+    // Delete message
+    await interaction.channel.bulkDelete(amount)
+      .then(async () => {
+        // Send success message
+        await interaction.reply({
+          content: `Successfully deleted ${amount} messages`,
+          ephemeral: true,
+        });
+      });
+  },
+};
