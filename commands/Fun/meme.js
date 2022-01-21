e463d9d5355c77407316e3c715b197f182350aea
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-01-21 21:29:47 +0700

    feat(cmd/misc): Add avatar command

--- commands/Fun/meme.js
+++ commands/Fun/meme.js
@@ -2,8 +2,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 // Import axios
 const axios = require("axios");
-// Import MessageEmbed from Discord.js
-const { MessageEmbed } = require("discord.js");
+// Import MessageEmbed, MessageButton and MessageActionRow from Discord.js
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 // Export command
 module.exports = {
@@ -23,10 +23,45 @@ module.exports = {
    */
   async execute(client, interaction) {
     // Fetch meme by axios
-    const { data } = await axios.get("https://meme-api.herokuapp.com/gimme");
+    let { data } = await axios.get("https://meme-api.herokuapp.com/gimme");
+    // Create new button
+    const button = new MessageActionRow().addComponents(
+      new MessageButton()
+        // Set button label
+        .setLabel("Next")
+        // Set button id
+        .setCustomId("next")
+        // Set button styles
+        .setStyle("PRIMARY"),
+    );
     // Send meme
     interaction.reply({
       embeds: [new MessageEmbed().setTitle(data.title).setImage(data.url).setURL(data.postLink).setColor("GREEN")],
+      components: [button],
+    });
+
+    // Filter for answer buttons
+    const filter = (i) =>
+      // Check if id is Confirm and if it is the same user
+      i.customId === "next" && i.user.id === interaction.user.id;
+
+    // Collect answer
+    const collector = interaction.channel.createMessageComponentCollector(filter, { time: 60000 });
+
+    // When answer is collected
+    collector.on("collect", async (i) => {
+      // Check if custom id is next
+      if (i.customId === "next") {
+        // Defer update
+        await i.deferUpdate();
+        // Fetch a new meme
+        let { data } = await axios.get("https://meme-api.herokuapp.com/gimme");
+        // Send meme
+        await interaction.editReply({
+          embeds: [new MessageEmbed().setTitle(data.title).setImage(data.url).setURL(data.postLink).setColor("GREEN")],
+          components: [button],
+        });
+      }
     });
   },
 };
