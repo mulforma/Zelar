e0999e6d71ecd567f1d25f29781e940cefd43367
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-02-19 09:27:09 +0700

    feat(cmd/coin-flip): Allow custom seed

--- commands/Game/coin-flip.js
+++ commands/Game/coin-flip.js
@@ -9,7 +9,15 @@ module.exports = {
     // Set command name
     .setName("coin-flip")
     // Set command description
-    .setDescription("Flips a coin and returns the result"),
+    .setDescription("Flips a coin and returns the result")
+    // Add string option
+    .addIntegerOption(option =>
+      option
+        // Set name
+        .setName("seed")
+        // Set description
+        .setDescription("Seed for the coin flip generated value")
+    ),
   // Set command category
   category: "Game",
   // Execute function
@@ -19,15 +27,26 @@ module.exports = {
    * @returns {Promise<void>}
    */
   async execute(client, interaction) {
-    let genNum = Math.ceil(Math.random() * 100);
-    let isEven = genNum % 2 === 0;
+    // Get seed
+    let seed = interaction.options.getInteger("seed") || new Date().getTime();
+    // Random number function
+    let randomNum = (seed) => {
+      let x = Math.sin(seed++) * 10000;
+      return x - Math.floor(x);
+    }
+    // Get random number
+    let genNum = randomNum(seed) * 100;
+    // Check if it's even
+    let isEven = Math.trunc(genNum) % 2 === 0;
 
+    // Reply with result
     await interaction.reply({
       embeds: [
         new MessageEmbed()
           .setTitle("🎲 Coin Flip")
           .addField("Result", isEven ? "Heads" : "Tails")
           .addField("Generated Number", String(genNum))
+          .addField("Seed", String(seed))
           .setColor(0x00ff00),
       ],
     });
