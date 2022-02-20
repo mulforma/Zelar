7f061d0d55d7a9963e123d418ada1274e4ec77cd
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-02-20 15:34:56 +0700

    feat(cmd/economics): Add fishing command

--- commands/Economics/inventory.js
+++ commands/Economics/inventory.js
@@ -1,5 +1,7 @@
 // Import SlashCommandBuilder
 const { SlashCommandBuilder } = require("@discordjs/builders");
+// Import MessageEmbed
+const { MessageEmbed } = require("discord.js");
 
 // Export command
 module.exports = {
@@ -18,19 +20,39 @@ module.exports = {
    * @returns {Promise<void>}
    */
   async execute(client, interaction) {
+    // Get user inventory
     client.db
       .select("inventory")
       .from("user")
-      .where("userId", interaction.message.author.id)
+      .where("userId", interaction.user.id)
       .then(async (result) => {
+        // If there is no inventory
         if (result[0].inventory.length === 0) {
+          // Reply with no inventory message
           await interaction.reply("You have no items in your inventory.");
         } else {
-          await interaction.reply(
-            `You have the following items in your inventory:\n${Object.keys(JSON.parse(result[0].inventory)).join(
-              ", ",
-            )}`,
-          );
+          // Reply with embeds whose show the inventory items
+          await interaction.reply({
+            embeds: [
+              new MessageEmbed()
+                .setAuthor({
+                  iconURL: interaction.user.displayAvatarURL(),
+                  name: `${interaction.user.username}'s inventory`,
+                })
+                .setDescription(
+                  result[0].inventory.items
+                    .map(
+                      (item) =>
+                        `${item.emoji} **${item.name}** — ${item.amount}\n*Item ID* \`${item.id}\` — ${
+                          item.type.split(".")[0]
+                        }`,
+                    )
+                    .join("\n\n"),
+                )
+                .setColor("GREEN")
+                .setFooter({ text: "Use `/use <item>` to use an item." }),
+            ],
+          });
         }
       });
   },
