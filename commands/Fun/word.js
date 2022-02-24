acb3b0431654f08d26940ec3ce5a995e4d6f5605
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-02-24 22:10:24 +0700

    refactor: Fix inner declarations

--- commands/Fun/word.js
+++ commands/Fun/word.js
@@ -14,28 +14,28 @@ module.exports = {
     // Set command description
     .setDescription("Plays a word game.")
     // Add subcommands
-    .addSubcommand(subcommand =>
+    .addSubcommand((subcommand) =>
       subcommand
         // Set subcommand name
         .setName("fill")
         // Set subcommand description
-        .setDescription("Fills in the blanks in a word.")
+        .setDescription("Fills in the blanks in a word."),
     )
     // Add subcommands
-    .addSubcommand(subcommand =>
+    .addSubcommand((subcommand) =>
       subcommand
         // Set subcommand name
         .setName("guess")
         // Set subcommand description
-        .setDescription("Guesses a word from its definition.")
+        .setDescription("Guesses a word from its definition."),
     )
     // Add subcommands
-    .addSubcommand(subcommand =>
+    .addSubcommand((subcommand) =>
       subcommand
         // Set subcommand name
         .setName("sort")
         // Set subcommand description
-        .setDescription("Sorts a shuffled word.")
+        .setDescription("Sorts a shuffled word."),
     ),
   // Set command category
   category: "Fun",
@@ -45,7 +45,20 @@ module.exports = {
    * @param {import("discord.js").CommandInteraction} interaction
    * @returns {Promise<void>}
    */
-  async execute (client, interaction) {
+  async execute(client, interaction) {
+    // GenBlanks function (For filling in the blanks)
+    function genBlanks(letter) {
+      // Make some letters underscores
+      const blankLetters = letter.map((letter) => (Math.random() > 0.33 ? letter : "\\_"));
+      // Check if word is already filled or none of the letters are revealed
+      if (blankLetters === letter || blankLetters.every((letter) => letter === "\\_")) {
+        // Generate new blanks
+        genBlanks();
+      } else {
+        // Return blanks
+        return blankLetters;
+      }
+    }
     // Get subcommands
     const subcommands = interaction.options.getSubcommand();
     // Switch subcommands
@@ -57,32 +70,23 @@ module.exports = {
         // Set if correct
         let correct = false;
         // Fetch word
-        const { data } = await axios.get("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt");
+        const { data } = await axios.get(
+          "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt",
+        );
         // Filter words that are less than 3 characters
-        const words = data.split("\n").filter(word => word.length > 2);
+        const words = data.split("\n").filter((word) => word.length > 2);
         // Get random word
         const word = words[Math.floor(Math.random() * words.length)];
         // Get split word
         const letters = word.split("");
-        // GenBlanks function
-        function genBlanks() {
-          // Make some letters underscores
-          let blankLetters = letters.map(letter => Math.random() > 0.33 ? letter : "\\_");
-          // Check if word is already filled or none of the letters are revealed
-          if (blankLetters === letters || blankLetters.every(letter => letter === "\\_")) {
-            // Generate new blanks
-            genBlanks();
-          } else {
-            // Return blanks
-            return blankLetters;
-          }
-        }
         // Get blanks
-        const blanks = genBlanks();
+        const blanks = genBlanks(letters);
         // Send message
         interaction.reply(`Fill in the blanks!\n\n${blanks.join(" ")}`);
         // Filter message
-        const filter = (message) => { return !message.author.bot && (message.author.id === interaction.user.id) };
+        const filter = (message) => {
+          return !message.author.bot && message.author.id === interaction.user.id;
+        };
         // Wait for message
         const collector = await interaction.channel.createMessageCollector({ filter, time: 30000 });
         // On collected message
@@ -108,7 +112,7 @@ module.exports = {
             // Send message
             interaction.channel.send(`❌ Incorrect! The correct answer was ${word}.`);
           }
-        })
+        });
         // On collector end
         collector.on("end", () => {
           // If not correct
@@ -117,6 +121,9 @@ module.exports = {
             interaction.channel.send(`⏰ Time's up! The correct answer was **${word}**.`);
           }
         });
+        break;
+      default:
+        break;
     }
   },
 };
