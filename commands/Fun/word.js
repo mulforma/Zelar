4541756314e61aacbcb4baf63e637a9d30867485
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-02-24 23:43:36 +0700

    feat(cmd/fun): Add word:sort commands

--- commands/Fun/word.js
+++ commands/Fun/word.js
@@ -25,14 +25,6 @@ module.exports = {
     .addSubcommand((subcommand) =>
       subcommand
         // Set subcommand name
-        .setName("guess")
-        // Set subcommand description
-        .setDescription("Guesses a word from its definition."),
-    )
-    // Add subcommands
-    .addSubcommand((subcommand) =>
-      subcommand
-        // Set subcommand name
         .setName("sort")
         // Set subcommand description
         .setDescription("Sorts a shuffled word."),
@@ -59,36 +51,38 @@ module.exports = {
         return blankLetters;
       }
     }
+    // Set tries
+    let tries = 0;
+    // Set if correct
+    let correct = false;
+    // Fetch word
+    const { data } = await axios.get(
+      "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt",
+    );
+    // Filter words that are less than 3 characters
+    const words = data.split("\n").filter((word) => word.length > 2);
+    // Get random word
+    const word = words[Math.floor(Math.random() * words.length)];
+    // Get split word
+    const letters = word.split("");
+    // Filter message
+    const filter = (message) => {
+      return !message.author.bot && message.author.id === interaction.user.id;
+    };
+    // Wait for message
+    const collector = await interaction.channel.createMessageCollector({ filter, time: 30000 });
+    // Generate random coin amount between 100 - 250
+    const amount = Math.floor(Math.random() * (250 - 100 + 1)) + 100;
     // Get subcommands
     const subcommands = interaction.options.getSubcommand();
     // Switch subcommands
     switch (subcommands) {
       // If subcommand is fill
       case "fill":
-        // Set tries
-        let tries = 0;
-        // Set if correct
-        let correct = false;
-        // Fetch word
-        const { data } = await axios.get(
-          "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt",
-        );
-        // Filter words that are less than 3 characters
-        const words = data.split("\n").filter((word) => word.length > 2);
-        // Get random word
-        const word = words[Math.floor(Math.random() * words.length)];
-        // Get split word
-        const letters = word.split("");
         // Get blanks
         const blanks = genBlanks(letters);
         // Send message
         interaction.reply(`Fill in the blanks!\n\n${blanks.join(" ")}`);
-        // Filter message
-        const filter = (message) => {
-          return !message.author.bot && message.author.id === interaction.user.id;
-        };
-        // Wait for message
-        const collector = await interaction.channel.createMessageCollector({ filter, time: 30000 });
         // On collected message
         collector.on("collect", (collected) => {
           // Get message
@@ -97,8 +91,43 @@ module.exports = {
           if (guess === word) {
             // Set correct
             correct = true;
-            // Generate random coin amount between 100 - 250
-            const amount = Math.floor(Math.random() * (250 - 100 + 1)) + 100;
+            // Send message
+            interaction.channel.send(`🎉 Correct! You won ${amount} coins!`);
+            // Add coins
+            addCoin(interaction, client.db, interaction.user.id, interaction.guild.id, amount);
+          } else if (tries < 3) {
+            // Add to tries
+            tries++;
+            // Send message
+            interaction.channel.send(`❌ Incorrect! You have ${3 - tries + 1} tries left.`);
+          } else {
+            // Send message
+            interaction.channel.send(`❌ Incorrect! The correct answer was ${word}.`);
+          }
+        });
+        // On collector end
+        collector.on("end", () => {
+          // If not correct
+          if (!correct && tries < 3) {
+            // Send message
+            interaction.channel.send(`⏰ Time's up! The correct answer was **${word}**.`);
+          }
+        });
+        break;
+      // If subcommand is sort
+      case "sort":
+        // Shuffle letters
+        letters.sort(() => Math.random() - 0.5);
+        // Send message
+        interaction.reply(`Sort the letters!\n\n${letters}`);
+        // On collected message
+        collector.on("collect", (collected) => {
+          // Get message
+          const guess = collected.content.toLowerCase();
+          // If guess is correct
+          if (guess === word) {
+            // Set correct
+            correct = true;
             // Send message
             interaction.channel.send(`🎉 Correct! You won ${amount} coins!`);
             // Add coins
