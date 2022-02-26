92e1d26aef538c9081d0321e5803525da121591e
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-02-26 20:42:13 +0700

    style: Run prettier format

--- commands/Image/magik.js
+++ commands/Image/magik.js
@@ -38,9 +38,7 @@ module.exports = {
     const avatar = user.displayAvatarURL({ format: "png", size: 512 });
     // GET request to nekobot api yet we are lazy to do it ourselves
     // TODO: Instead of using other's api, we should use our own technique. This will improve performance much better.
-    const { data } = await axios.get(
-      `https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}&intensity=1`,
-    );
+    const { data } = await axios.get(`https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}&intensity=1`);
     // Send image
     await interaction.editReply({
       files: [
