634e32c29a8b2a8678a3157aee771b3ed6b61a4d
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-20 23:50:30 +0700

    refactor: convert index file to typescript

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 // Import dotenv
-require("dotenv").config();
+import "dotenv/config";
 // Import ShardingManager from discord.js
-const { ShardingManager } = require("discord.js");
+import { ShardingManager } from "discord.js";
 // Import npmlog
-const log = require("npmlog");
+import * as log from "npmlog";
 
 // Create a new ShardingManager
 const manager = new ShardingManager("./bot.js", { token: process.env.TOKEN });
@@ -11,13 +11,13 @@ const manager = new ShardingManager("./bot.js", { token: process.env.TOKEN });
 // Listen for shard events
 manager.on("shardCreate", (shard) => {
   // Log shards
-  log.info(`Launched shard ${shard.id}`);
+  log.info("", `Launched shard ${shard.id}`);
   // On shard error
   shard.on("error", (error) => {
     // Log error
-    log.error(`Shard ${shard.id} errored: ${error}`);
+    log.error("", `Shard ${shard.id} errored: ${error}`);
   });
 });
 
 // Spawn shards
-manager.spawn({ amount: "auto" });
+manager.spawn({ amount: "auto" }).then();
