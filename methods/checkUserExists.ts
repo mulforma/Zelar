1128e5b21a1adb1658a5b67011b506e26d6f522f
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-21 00:19:06 +0700

    refactor: convert checkUserExists file to typescript

--- methods/checkUserExists.ts
+++ methods/checkUserExists.ts
@@ -1,14 +1,20 @@
-module.exports = (interaction, db, userId, guildId) => {
-  db.select("*")
-    .from("user")
+// Import CommandInteraction
+import { CommandInteraction } from "discord.js";
+// Import Knex
+import { Knex } from "knex";
+
+exports = (interaction: CommandInteraction, db: Knex, userId: number, guildId: number) => {
+  db("user")
+    .select("*")
     .where("userId", userId)
     .andWhere("serverId", guildId)
+    // @ts-ignore
     .then((user) => {
       if (user.length <= 0) {
         // Check if interaction has been replied to
         if (interaction.replied) {
           // If so, send channel message
-          return interaction.channel.send("You don't have a profile yet! Type `/profile` to create one!");
+          return interaction.channel?.send("You don't have a profile yet! Type `/profile` to create one!");
         }
         // If not, reply to interaction
         return interaction.reply("You don't have a profile yet! Type `/profile` to create one!");
