33c1e904d9b990245e3946d8482de5dfb9f74729
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-21 10:28:15 +0700

    refactor: convert daily command to typescript

--- commands/Economics/daily.ts
+++ commands/Economics/daily.ts
@@ -1,11 +1,12 @@
 // Import SlashCommandBuilder
-const { SlashCommandBuilder } = require("@discordjs/builders");
+import { SlashCommandBuilder } from "@discordjs/builders";
 // Import getUserData
-const getUserData = require("../../methods/getUserData");
+import { getUserData } from "../../methods/getUserData";
 // Import addCoin
-const addCoin = require("../../methods/addCoin");
+import { addCoin } from"../../methods/addCoin";
 // Import ms
-const ms = require("ms");
+import ms from "ms";
+import { Client, CommandInteraction } from "discord.js";
 
 // Export command
 module.exports = {
@@ -18,14 +19,9 @@ module.exports = {
   // Set command category
   category: "Economics",
   // Execute function
-  /**
-   * @param {import('discord.js').Client} client
-   * @param {import('discord.js').CommandInteraction} interaction
-   * @returns {Promise<void>}
-   */
-  async execute(client, interaction) {
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
     // Get user data
-    const userData = await getUserData(interaction, client.db, interaction.user.id, interaction.guild.id);
+    const userData = await getUserData(interaction, client.db, interaction.user.id, interaction.guild!.id);
     // Get user daily coins time
     const lastDaily = Number(userData.timeout.daily);
     // Get current date
@@ -38,12 +34,12 @@ module.exports = {
     // If user lasted daily coin time is less than a day
     if (currentDate - lastDaily < day) {
       // Send message
-      interaction.reply(
+      await interaction.reply(
         `You already got your daily coins!\nYou can get them again in ${ms(day - (currentDate - lastDaily))}`,
       );
     } else {
       // Add daily coins
-      await addCoin(interaction, client.db, interaction.user.id, interaction.guild.id, dailyAmount);
+      await addCoin(interaction, client.db, interaction.user.id, interaction.guild!.id, dailyAmount);
       // Set new daily time
       userData.timeout.daily = currentDate;
       // Save user data
@@ -51,9 +47,9 @@ module.exports = {
         .db("user")
         .update("timeout", userData.timeout)
         .where("userId", interaction.user.id)
-        .where("serverId", interaction.guild.id);
+        .where("serverId", <string>interaction.guild?.id);
       // Send message
-      interaction.reply(`You got ${dailyAmount} coins.\nNow you have ${Number(userData.coin) + dailyAmount} coins.`);
+      await interaction.reply(`You got ${dailyAmount} coins.\nNow you have ${Number(userData.coin) + dailyAmount} coins.`);
     }
   },
 };
