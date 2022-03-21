131e4ad4c46544dd2b69007e142f6d04d0b1cf5c
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-21 14:34:40 +0700

    refactor: convert trackAdd event to typescript

--- players/trackAdd.ts
+++ players/trackAdd.ts
@@ -1,10 +1,10 @@
-// Import Queue
-import { Queue } from "discord-player";
+// Import Queue and Track
+import { Queue, Track } from "discord-player";
 
-module.exports = {
+export default {
   name: "trackAdd",
   description: "Triggered when a track is added to the queue.",
-  async run(queue, track) {
+  async run(queue: Queue<any>, track: Track) {
     await queue.metadata.channel.send({
       content: `[🎧] **${track.title}** has been added to the queue.`,
     });
