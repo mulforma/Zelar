1fa70a18a6d3205ecd2c1a595e8e14333e4acd4e
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-21 14:35:16 +0700

    refactor: convert trackStart event to typescript

--- players/trackStart.ts
+++ players/trackStart.ts
@@ -1,10 +1,10 @@
-// Import Queue
-import { Queue } from "discord-player";
+// Import Queue and Track
+import { Queue, Track } from "discord-player";
 
 module.exports = {
   name: "trackStart",
   description: "Triggered when a track starts playing",
-  async run(queue, track) {
+  async run(queue: Queue<any>, track: Track) {
     if (queue.repeatMode !== 0) return;
     await queue.metadata.channel.send({
       content: `[🎧] Playing ${track.title}`,
