3173f6ba5931dc0dd5e8c6a5b26e74589675614a
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-25 23:51:09 +0700

    refactor: convert now-playing command to typescript

---
+++ commands/Music/now-playing.ts
@@ -0,0 +1,48 @@
+// Import SlashCommandBuilder
+import { SlashCommandBuilder } from "@discordjs/builders";
+// Import Client and CommandInteraction
+import { Client, CommandInteraction, MessageEmbed } from "discord.js";
+
+// Export command
+export default {
+  // Set command data
+  data: new SlashCommandBuilder()
+    // Set command name
+    .setName("now-playing")
+    // Set command description
+    .setDescription("Shows the current song playing."),
+  // Set command category
+  category: "Music",
+  // Execute function
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Get queue
+    const queue = client.player.getQueue(interaction.guild!.id);
+
+    // If queue is empty
+    if (!queue) {
+      // Send error message
+      return interaction.reply({
+        content: "There is no song playing.",
+      });
+    }
+
+    // Loop methods
+    const loopMethods = ["None", "Track", "Queue"];
+
+    // Create embed
+    const embed = new MessageEmbed()
+      // Set title
+      .setTitle(queue.current.title)
+      // Set URLs
+      .setURL(queue.current.url)
+      // Set thumbnail
+      .setThumbnail(queue.current.thumbnail)
+      // Set description
+      .setDescription(`${queue.current.author} - ${queue.current.duration} (Loop ${loopMethods[queue.repeatMode]})`)
+      // Set footer
+      .setFooter({ text: `Requested by ${queue.current.requestedBy.tag}` });
+
+    // Send embed
+    await interaction.reply({ embeds: [embed] });
+  },
+};
