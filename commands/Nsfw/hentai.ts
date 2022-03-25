a0803fded77edf2ae04175da3a52910eecd81fe7
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-26 00:01:46 +0700

    refactor: convert hentai command to typescript

---
+++ commands/Nsfw/hentai.ts
@@ -0,0 +1,78 @@
+// Import SlashCommandBuilder
+import { SlashCommandBuilder } from "@discordjs/builders";
+// Import axios
+import axios from "axios";
+// Import MessageEmbed
+import { Client, CommandInteraction, MessageEmbed, TextChannel } from "discord.js";
+
+// Export command
+export default {
+  // Set command data
+  data: new SlashCommandBuilder()
+    // Set command name
+    .setName("hentai")
+    // Set command description
+    .setDescription("Sends a random hentai image (from gelbooru)")
+    // Add string option
+    .addStringOption((option) =>
+      option
+        // Set option name
+        .setName("tag")
+        // Set option description
+        .setDescription("The tag to search for (Separate with commas)")
+        // Set option required
+        .setRequired(false),
+    ),
+  // Set command category
+  category: "Nsfw",
+  // Execute function
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Check if channel is NSFW
+    if (!(<TextChannel>interaction.channel)!.nsfw) {
+      // Send error message
+      await interaction.reply({
+        embeds: [new MessageEmbed().setColor("#ff0000").setDescription("This channel is not NSFW")],
+      });
+      // Return
+      return;
+    }
+
+    // Get tags
+    const tags = <Array<string> | string>(interaction.options.getString("tag")
+      ? // If tag is undefined
+        interaction.options.getString("tag")
+      : // Else
+        "");
+
+    // Get random page
+    const page = Math.floor(Math.random() * 100) + 1;
+
+    // Get random image
+    const image = await axios.get(
+      `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&pid=${page}${
+        tags ? `&tags=${(<Array<string>>tags).join("+")}` : ""
+      }`,
+    );
+
+    // If no image
+    if (!image.data.post[0]) {
+      // Send error message
+      await interaction.reply({
+        embeds: [new MessageEmbed().setColor("#ff0000").setDescription("No image found")],
+      });
+      return;
+    }
+
+    // Send image
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(`Image from ${image.data.post[0].owner}`)
+          .setColor("#ffb6c1")
+          .setImage(image.data.post[0].file_url)
+          .setFooter({ text: `rating: ${image.data.post[0].rating} | score: ${image.data.post[0].score}` })
+          .setURL(`https://gelbooru.com/index.php?page=post&s=view&id=${image.data.post[0].id}`),
+      ],
+    });
+  },
+};
