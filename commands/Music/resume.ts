e6c01547dceb998dba86d96cc7e9bd2896e652e8
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-25 23:55:29 +0700

    refactor: convert resume command to typescript

---
+++ commands/Music/resume.ts
@@ -0,0 +1,43 @@
+// Import SlashCommandBuilder
+import { SlashCommandBuilder } from "@discordjs/builders";
+// Import Client and CommandInteraction
+import { Client, CommandInteraction } from "discord.js";
+
+// Export command
+export default {
+  // Set command data
+  data: new SlashCommandBuilder()
+    // Set command name
+    .setName("resume")
+    // Set command description
+    .setDescription("Resume the current song."),
+  // Set command category
+  category: "Music",
+  // Execute function
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Get queue
+    const queue = client.player.getQueue(interaction.guild!.id);
+
+    // Check if queue is empty
+    if (!queue) {
+      // Send error message
+      return interaction.reply({
+        content: "There is no song playing.",
+      });
+    }
+
+    // Pause queue
+    const success = queue.setPaused(false);
+
+    // Check if queue was paused
+    if (success) {
+      // Send success message
+      return interaction.reply({
+        content: "The song has been resumed.",
+      });
+    }
+    return interaction.reply({
+      content: "There was an error resuming the song.",
+    });
+  },
+};
