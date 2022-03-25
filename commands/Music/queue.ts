40239969e786f6a7865a31affde0c5588b7b0fea
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-25 23:54:34 +0700

    refactor: convert queue command to typescript

---
+++ commands/Music/queue.ts
@@ -0,0 +1,55 @@
+// Import SlashCommandBuilder
+import { SlashCommandBuilder } from "@discordjs/builders";
+// Import MessageEmbed from Discord.js
+import { MessageEmbed } from "discord.js";
+// Import Client and CommandInteraction
+import { Client, CommandInteraction } from "discord.js";
+
+// Export command
+export default {
+  // Set command data
+  data: new SlashCommandBuilder()
+    // Set command name
+    .setName("queue")
+    // Set command description
+    .setDescription("Shows the current queue."),
+  // Set command category
+  category: "Music",
+  // Execute function
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Get queue
+    const queue = client.player.getQueue(interaction.guild!.id);
+
+    // If queue is empty
+    if (!queue) {
+      // Send error message
+      return interaction.reply({
+        embeds: [
+          new MessageEmbed().setColor("#ff0000").setDescription("There is no queue.")
+        ],
+      });
+    }
+
+    // If there is no next song
+    if (!queue.tracks[0]) {
+      // Send error message
+      return interaction.reply({
+        embeds: [
+          new MessageEmbed().setColor("#ff0000").setDescription("There is no queue after this song.")]
+        ,
+      });
+    }
+
+    // Create embed
+    const embed = new MessageEmbed()
+      .setColor("#00ff00")
+      .setTitle("Queue")
+      .setDescription(
+        `${queue.tracks.map((track, index) => `${index + 1}. [${track.title}](${track.url})`).join("\n")}`,
+      )
+      .setThumbnail(client.user!.displayAvatarURL());
+
+    // Send embed
+    await interaction.reply({ embeds: [embed] });
+  },
+};
