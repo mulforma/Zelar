65b2ad3fbbffaf351c6b951b1af78f6fb19836ff
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-26 09:53:48 +0700

    refactor: convert inventory command to typescript

---
+++ commands/Profile/inventory.ts
@@ -0,0 +1,58 @@
+// Import SlashCommandBuilder
+import { SlashCommandBuilder } from "@discordjs/builders";
+// Import MessageEmbed
+import { MessageEmbed } from "discord.js";
+// Import Client and CommandInteraction
+import { Client, CommandInteraction } from "discord.js";
+// Import InventoryItemData
+import { InventoryItemData } from "../../types/InventoryItemData";
+
+// Export command
+export default {
+  // Set command data
+  data: new SlashCommandBuilder()
+    // Set command name
+    .setName("inventory")
+    // Set command description
+    .setDescription("View your current inventory."),
+  // Set command category
+  category: "Profile",
+  // Execute function
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Get user inventory
+    client.db
+      .select("inventory")
+      .from("user")
+      .where("userId", interaction.user.id)
+      .then(async (result) => {
+        // If there is no inventory
+        if (result[0].inventory.length === 0) {
+          // Reply with no inventory message
+          await interaction.reply("You have no items in your inventory.");
+        } else {
+          // Reply with embeds whose show the inventory items
+          await interaction.reply({
+            embeds: [
+              new MessageEmbed()
+                .setAuthor({
+                  iconURL: interaction.user.displayAvatarURL(),
+                  name: `${interaction.user.username}'s inventory`,
+                })
+                .setDescription(
+                  result[0].inventory.items
+                    .map(
+                      (item: InventoryItemData) =>
+                        `${item.emoji} **${item.name}** — ${item.amount}\n*Item ID* \`${item.id}\` — ${
+                          item.type.split(".")[0]
+                        }`,
+                    )
+                    .join("\n\n"),
+                )
+                .setColor("GREEN")
+                .setFooter({ text: "Use `/use <item>` to use an item." }),
+            ],
+          });
+        }
+      });
+  },
+};
