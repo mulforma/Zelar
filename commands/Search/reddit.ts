50d00a11d741927607fc2856da8a2ecc079afb18
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-26 10:36:14 +0700

    refactor: convert reddit command to typescript

---
+++ commands/Search/reddit.ts
@@ -0,0 +1,48 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import axios from "axios";
+import { Client, CommandInteraction, MessageEmbed } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("reddit")
+    .setDescription("Search for a subreddit")
+    .addStringOption((option) =>
+      // Set option name
+      option.setName("subreddit").setDescription("The subreddit to search").setRequired(true),
+    ),
+  category: "Search",
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Get subreddit
+    const subreddit = interaction.options.getString("subreddit");
+
+    // Fetches the subreddit
+    try {
+      const { data } = await axios.get(`https://www.reddit.com/r/${subreddit}.json`);
+
+      // Get random post
+      const post = data.data.children[Math.floor(Math.random() * data.data.children.length)].data;
+
+      // Create embed
+      const embed = new MessageEmbed()
+        // Set title
+        .setTitle(`${post.title}`)
+        .setDescription(`${post.selftext}`)
+        // Set footer
+        .setFooter({ text: post.subreddit_name_prefixed })
+        // Set URL
+        .setURL(`https://reddit.com${post.permalink}`)
+        // Set timestamp
+        .setTimestamp(new Date(post.created_utc * 1000))
+        // Set image
+        .setImage(post.url || post.thumbnail);
+
+      // Send embed
+      await interaction.reply({ embeds: [embed] });
+    } catch (e) {
+      // Send error message
+      return interaction.reply({
+        embeds: [new MessageEmbed().setColor("#ff0000").setTitle("Error").setDescription("Subreddit not found")],
+      });
+    }
+  },
+};
