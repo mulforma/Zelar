0d8ca7c9fd164e894b664850855f19910fa74b5f
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-26 09:59:57 +0700

    refactor: convert jobs command to typescript

---
+++ commands/Profile/jobs.ts
@@ -0,0 +1,140 @@
+// Import SlashCommandBuilder
+import { SlashCommandBuilder } from "@discordjs/builders";
+// Import MessageEmbed
+import { Client, CommandInteraction, MessageEmbed } from "discord.js";
+// Import getUserData
+import { getUserData } from "../../methods/getUserData";
+// Import checkTimeout
+import { checkTimeout } from "../../methods/checkTimeout";
+// Import ms
+import ms from "ms";
+// Import TimeoutCommandData
+import { TimeoutCommandData } from "../../types/UserData";
+
+// Export command
+export default {
+  // Set command data
+  data: new SlashCommandBuilder()
+    // Set command name
+    .setName("jobs")
+    // Set command description
+    .setDescription("Choose a job")
+    // Add string option
+    .addStringOption((option) =>
+      option
+        // Set option name
+        .setName("job")
+        // Set option description
+        .setDescription("The job you want to choose")
+        // Set option required
+        .setRequired(false),
+    ),
+  // Set command category
+  category: "Profile",
+  // Execute function
+  async execute(client: Client, interaction: CommandInteraction): Promise<void> {
+    // Go get a job
+    const job = interaction.options.getString("job");
+    // Get user data
+    const user = await getUserData(interaction, client.db, interaction.user.id, interaction.guild?.id ?? "");
+    // Get all jobs
+    const jobs = await client.db.select("*").from("jobs");
+    // Check if job is exists
+    if (!job) {
+      // Create embed
+      const embed = new MessageEmbed()
+        // Set title
+        .setTitle("Jobs")
+        // Set description
+        .setDescription(
+          jobs
+            .map(
+              (j) =>
+                `${Number(user.level) < Number(j.minimumLevel) ? "🔒" : "✅"} **${j.name}** - *${
+                  j.description
+                }*\nIncome: $${j.income}`,
+            )
+            .join("\n\n"),
+        )
+        // Set color
+        .setColor("RED");
+      // Send embed
+      await interaction.reply({ embeds: [embed] });
+    } else {
+      // Check if job is valid
+      if (!jobs.find((j) => j.name.toLowerCase() === job.toLowerCase())) {
+        // Create embed
+        const embed = new MessageEmbed()
+          // Set title
+          .setTitle("Jobs")
+          // Set description
+          .setDescription(`The job **${job}** does not exist!`)
+          // Set color
+          .setColor("RED");
+        // Send embed
+        await interaction.reply({ embeds: [embed] });
+      } else {
+        // Check if user has enough level
+        if (Number(user.level) < Number(jobs.find((j) => j.name.toLowerCase() === job.toLowerCase()).minimumLevel)) {
+          // Create embed
+          const embed = new MessageEmbed()
+            // Set title
+            .setTitle("Jobs")
+            // Set description
+            .setDescription(
+              `You need to be at least level ${
+                jobs.find((j) => j.name.toLowerCase() === job.toLowerCase()).minimumLevel
+              } to choose this job!`,
+            )
+            // Set color
+            .setColor("RED");
+          // Send embed
+          await interaction.reply({ embeds: [embed] });
+          // Check if user already choose this job
+        } else if (user.job === job) {
+          // Create embed
+          const embed = new MessageEmbed()
+            // Set title
+            .setTitle("Jobs")
+            // Set description
+            .setDescription("You already choose this job!")
+            // Set color
+            .setColor("RED");
+          // Send embed
+          await interaction.reply({ embeds: [embed] });
+        } else {
+          // One week in milliseconds
+          const week = ms("7d");
+          // Check user timeout
+          if (await checkTimeout(interaction, client.db, "jobs", week, user)) {
+            return;
+          }
+          // Update user job
+          await client.db("user").update({ jobs: job }).where({ userId: interaction.user.id });
+          // Create embed
+          const embed = new MessageEmbed()
+            // Set title
+            .setTitle("Jobs")
+            // Set description
+            .setDescription(`You have chosen the job **${job}**!`)
+            // Set color
+            .setColor("RED");
+          // Send embed
+          await interaction.reply({ embeds: [embed] });
+
+          // Check if user already has timeout
+          if (user.timeout.findIndex((x: TimeoutCommandData) => x.command === "jobs") !== -1) {
+            // Update timeout
+            user.timeout[user.timeout.findIndex((x: TimeoutCommandData) => x.command === "jobs")].time = Date.now();
+          } else {
+            // Add timeout
+            user.timeout.push({ command: "jobs", time: Date.now() });
+          }
+
+          // Update user timeout
+          await client.db("user").update({ timeout: user.timeout }).where({ userId: interaction.user.id });
+        }
+      }
+    }
+  },
+};
