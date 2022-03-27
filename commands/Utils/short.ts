c516260b7ce14269a5e51ff235428f776fd595ea
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-27 10:25:40 +0700

    feat: make short command ephemeral

--- commands/Utils/short.ts
+++ commands/Utils/short.ts
@@ -10,14 +10,18 @@ export default {
   category: "Utils",
   async execute(client: Client, interaction: CommandInteraction): Promise<void> {
     // Get url
-    const URL = interaction.options.getString("url");
+    const url = interaction.options.getString("url");
     // Deferred reply
-    await interaction.deferReply();
+    await interaction.deferReply({
+      ephemeral: true
+    });
 
     // Shorten url with x.vvx.bar
-    axios.post("https://x.vvx.bar/create/url", { URL }).then(async (res) => {
+    axios.post("https://x.vvx.bar/create/url", { url }).then(async (res) => {
       // Send response
-      await interaction.editReply(`${URL} -> https://x.vvx.bar/${res.data.short_url}`);
+      await interaction.editReply({
+        content: `https://x.vvx.bar/${res.data.short_url}`
+      });
     }).catch(async (reason) => {
       // Send error
       await interaction.editReply(`An error occurred while shortening your url.\nReason: ${reason}`);
