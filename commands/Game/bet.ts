7f6f49e0d345eb3e471cfb5a805423bb643f0902
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-07-16 11:54:12 +0700

    fix(cmd/bet): dis-allowed negative numbers

--- commands/Game/bet.ts
+++ commands/Game/bet.ts
@@ -8,8 +8,8 @@ export default {
   data: new SlashCommandBuilder()
     .setName("bet")
     .setDescription("Bet on a game.")
-    .addIntegerOption((option) => option.setName("amount").setDescription("The amount to bet.").setRequired(true))
-    .addIntegerOption((option) =>
+    .addNumberOption((option) => option.setName("amount").setDescription("The amount to bet.").setRequired(true))
+    .addNumberOption((option) =>
       option.setName("number").setDescription("The number to bet (1-10).").setRequired(true),
     ),
   category: "Game",
@@ -19,7 +19,9 @@ export default {
     // Timestamp
     const timestamp = Date.now();
     // Get amount
-    const amount = interaction.options.getInteger("amount")!;
+    const amount = interaction.options.getNumber("amount")!;
+    // Get guess number
+    const guess = interaction.options.getNumber("number")!;
     // Get user
     const user = await getUserData(interaction, interaction.user.id, interaction.guild!.id);
     // Check if user has enough money
@@ -29,13 +31,13 @@ export default {
     }
 
     // Check if number is valid
-    if (interaction.options.getInteger("number")! < 1 || interaction.options.getInteger("number")! > 10) {
+    if (amount < 1 || guess > 10 || guess < 1) {
       // Send error message
       return interaction.reply("The number must be between 1 and 10.");
     }
 
     // Check if bet number is equal to random number
-    if (interaction.options.getInteger("number") === number) {
+    if (guess === number) {
       // Add money to user
       await addCoin(interaction, interaction.user.id, interaction.guild!.id, amount * 2);
       // Send success message
