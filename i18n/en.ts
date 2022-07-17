f419e4947113591540e4b8c1247e90122d5fcab4
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-07-17 22:01:08 +0700

    Add i18n for English and Thai languague

---
+++ i18n/en.ts
@@ -0,0 +1,12 @@
+export default {
+  en: {
+    translation: {
+      error: {
+        unspecified: "Oh no! Something went wrong, try again later.",
+        no_coin: "Oops! You don't have any coin!",
+        not_enough_coin: "Sorry! You don't have enough coin.",
+        not_enough_coin_shop: "Sorry! You don't have enough coin to buy **{{item}}**. You need **{{diff}}** more coin!",
+      },
+    },
+  },
+};
